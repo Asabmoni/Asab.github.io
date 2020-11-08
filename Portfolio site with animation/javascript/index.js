733e3924a8e3d8075ad79ed6553d9cdd04ec5dc0
Richard Asab <62440560+Asabmoni@users.noreply.github.com>
2020-11-08 14:09:47 +0000

    organize files

---
+++ Portfolio site with animation/javascript/index.js
@@ -0,0 +1,13 @@
+const portfolioItems = document.querySelectorAll('.singleitem');
+
+portfolioItems.forEach((portfolioItem) => {
+	portfolioItem.addEventListener('mouseover', () => {
+		console.log(portfolioItem.childNodes[1].classList);
+		portfolioItem.childNodes[1].classList.add('img-darken');
+	});
+
+	portfolioItem.addEventListener('mouseout', () => {
+		console.log(portfolioItem.childNodes[1].classList);
+		portfolioItem.childNodes[1].classList.remove('img-darken');
+	});
+});
